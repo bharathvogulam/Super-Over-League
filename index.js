206a0c4d48f50726b504fea6c0fac7f40c9524b5
bharathreddy <bharathreddyvogulam99@gmail.com>
2022-11-09 08:56:49 +0000

    fresh

---
+++ index.js
@@ -0,0 +1,18 @@
+const element = (
+  <div class="csscolors">
+    <h1>Super Over League</h1>
+    <div>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        class="firstImg"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        class="secondImg"
+      />
+    </div>
+  </div>
+  //   Write your code here.
+);
+
+ReactDOM.render(element, document.getElementById("root"));
